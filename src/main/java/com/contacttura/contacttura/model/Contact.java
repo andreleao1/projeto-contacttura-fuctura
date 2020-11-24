2a9bb7a97b6936ea27b947da2a07963c799e03f3
andreleao1 <andre.gustavo.ls@hotmail.com>
2020-11-24 12:16:46 -0300

    Implementação da classe Contact

---
+++ src/main/java/com/contacttura/contacttura/model/Contact.java
@@ -0,0 +1,27 @@
+package com.contacttura.contacttura.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Contact {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+
+	private String name;
+
+	private String email;
+
+	private String phone;
+
+}
