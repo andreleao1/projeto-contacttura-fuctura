75f8cb46c9d5a245a6becfd6931cec3a12747b7f
andreleao1 <andre.gustavo.ls@hotmail.com>
2020-11-24 12:30:57 -0300

    adição da anotação service

--- src/main/java/com/contacttura/contacttura/service/ContactService.java
+++ src/main/java/com/contacttura/contacttura/service/ContactService.java
@@ -1,10 +1,12 @@
 package com.contacttura.contacttura.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.contacttura.contacttura.model.Contact;
 import com.contacttura.contacttura.repository.ContactRepository;
 
+@Service
 public class ContactService implements IContactService{
 
 	@Autowired
