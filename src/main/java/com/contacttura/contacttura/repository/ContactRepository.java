7c848f37e5142e1b4bd109c368c18536d2cbf58d
andreleao1 <andre.gustavo.ls@hotmail.com>
2020-11-24 12:22:04 -0300

    Implementação da interface ContactRespository

---
+++ src/main/java/com/contacttura/contacttura/repository/ContactRepository.java
@@ -0,0 +1,11 @@
+package com.contacttura.contacttura.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.contacttura.contacttura.model.Contact;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+}
